7fea602bf638345f17342957292ca964928384b7
liutaochange <liutaochange@163.com>
2018-02-06 18:57:29 +0800

    publish

--- src/store/index.js
+++ src/store/index.js
@@ -5,50 +5,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
+import playMode from './player.js'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    audio: {
-      'id': 0,
-      'name': '歌曲名称',
-      'singer': '演唱者',
-      'albumPic': '/static/placeholder_disk_play_program.png',
-      'location': '',
-      'album': ''
-    },
-    lyric: '',
-    currentIndex: 0, // 当前播放的歌曲位置
-    playing: false, // 是否正在播放
-    loading: false, // 是否正在加载中
-    showDetail: false,
-    songList: [],    // 播放列表
-    currentTime: 0,
-    tmpCurrentTime: 0,
-    durationTime: 0,
-    bufferedTime: 0,
-    change: false,   // 判断是更改的时间还是播放的时间
+    //控制首页tab索引
     home_index: "recommend",
-    index_value: "findmusic"
-  },
-  getters: {
-    audio: state => state.audio,
-    playing: state => state.playing,
-    loading: state => state.loading,
-    showDetail: state => state.showDetail,
-    durationTime: state => state.durationTime,
-    currentIndex: state => state.currentIndex,
-    bufferedTime: state => state.bufferedTime,
-    tmpCurrentTime: state => state.tmpCurrentTime,
-    songList: state => state.songList,
-    change: state => state.change,
-    currentTime: state => state.currentTime,
-    prCurrentTime: state => {
-      return state.currentTime / state.durationTime * 100
-    },
-    prBufferedTime: state => {
-      return state.bufferedTime / state.durationTime * 100
-    }
+    index_value: "findmusic",
+    //音乐播放组件
+    singer:{},
+    playing:false,    //是否播放
+    fullScreen:false,    //是否全屏
+    playList:[],   //播放列表
+    sequenceList:[],    // 非顺序播放列表
+    mode:playMode.sequence,   // 播放模式（顺序0，循环1，随机2）
+    currentIndex:-1,  //当前播放索引
   },
   mutations: {
     setIndex(state,index){
@@ -56,113 +28,6 @@ const store = new Vuex.Store({
     },
     setIndexValue(state,index){
       state.index_value = index;
-    },
-    play (state) {
-      state.playing = true
-    },
-    pause (state) {
-      state.playing = false
-    },
-    toggleDetail (state) {
-      state.showDetail = !state.showDetail
-    },
-    setAudio (state) {
-      state.audio = state.songList[state.currentIndex - 1]
-    },
-    setAudioIndex (state, index) {
-      state.audio = state.songList[index]
-      state.currentIndex = index + 1
-    },
-    removeAudio (state, index) {
-      state.songList.splice(index, 1)
-      if (index === state.songList.length) {
-        index--
-      }
-      state.audio = state.songList[index]
-      state.currentIndex = index + 1
-      if (state.songList.length === 0) {
-        state.audio = {
-          'id': 0,
-          'name': '歌曲名称',
-          'singer': '演唱者',
-          'albumPic': '/static/player-bar.png',
-          'location': '',
-          'album': ''
-        }
-        state.playing = false
-      }
-    },
-    setChange (state, flag) {
-      state.change = flag
-    },
-    setLocation (state, location) {
-      state.audio.location = location
-    },
-    updateCurrentTime (state, time) {
-      state.currentTime = time
-    },
-    updateDurationTime (state, time) {
-      state.durationTime = time
-    },
-    updateBufferedTime (state, time) {
-      state.bufferedTime = time
-    },
-    changeTime (state, time) {
-      state.tmpCurrentTime = time
-    },
-    openLoading (state) {
-      state.loading = true
-    },
-    closeLoading (state) {
-      state.loading = false
-    },
-    resetAudio (state) {
-      state.currentTime = 0
-    },
-    playNext (state) { // 播放下一曲
-      state.currentIndex++
-      if (state.currentIndex > state.songList.length) {
-        state.currentIndex = 1
-      }
-      state.audio = state.songList[state.currentIndex - 1]
-    },
-    playPrev (state) { // 播放上一曲
-      state.currentIndex--
-      if (state.currentIndex < 1) {
-        state.currentIndex = state.songList.length
-      }
-      state.audio = state.songList[state.currentIndex - 1]
-    },
-    addToList (state, songs) {
-      var items = Array.prototype.concat.call(songs)
-      items.forEach(item => {
-        var flag = false
-        state.songList.forEach(function (element, index) { // 检测歌曲重复
-          if (element.id === item.id) {
-            flag = true
-            state.currentIndex = index + 1
-          }
-        })
-        if (!flag) {
-          state.songList.push(item)
-          state.currentIndex = state.songList.length
-        }
-      })
-    },
-    setLrc (state, lrc) {
-      state.lyric = lrc
-    }
-  },
-  // 异步的数据操作
-  actions: {
-    getSong ({commit, state}, id) {
-      // 使用 CancelToken 退出一个Axios事件
-      var CancelToken = Axios.CancelToken
-      var source = CancelToken.source()
-      if (state.loading && state.songList.length > 0) {
-        console.log('cancel')
-        source.cancel()
-      }
     }
   }
 })
